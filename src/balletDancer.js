e0671cb409a1a1b8280fa6a60b70d89ca4041a02
daartv <daartv@gmail.com>
2017-02-13 20:26:55 -0800

    Progress on BlinkyDancer

---
+++ src/balletDancer.js
@@ -0,0 +1,16 @@
+/*var makeBalletDancer = function(top, left, timeBetweenSteps) {
+ 
+  makeDancer.call(this, top, left, timeBetweenSteps);
+
+};
+
+makeBalletDancer.prototype = Object.create(makeDancer.prototype);
+
+makeBalletDancer.prototype.constructor = makeBalletDancer;
+
+makeBalletDancer.prototype.step = function() {
+    
+  makeDancer.prototype.step.call(this);
+
+  this.$node.toggle();
+};*/
---
+++ src/hiphopDancer.js
@@ -0,0 +1,16 @@
+/*var makeHiphopDancer = function(top, left, timeBetweenSteps) {
+ 
+  makeDancer.call(this, top, left, timeBetweenSteps);
+
+};
+
+makeHiphopDancer.prototype = Object.create(makeDancer.prototype);
+
+makeHiphopDancer.prototype.constructor = makeHiphopDancer;
+
+makeHiphopDancer.prototype.step = function() {
+    
+  makeDancer.prototype.step.call(this);
+
+  this.$node.toggle();
+};*/
